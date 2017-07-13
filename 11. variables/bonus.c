51a9ed3249108e99a7ef3a0eb09d5780788c5254
bannified <freedomzelda_dc@hotmail.com>
2017-07-14 07:22:53 +0800

    Done with 11. Variables excl. Bonus.c

---
+++ 11. variables/bonus.c
@@ -0,0 +1,8 @@
+› Create a Macro to aid specifically with reporting type errors.
+› Create a Macro to aid specifically with reporting argument count errors.
+› Create a Macro to aid specifically with reporting empty list errors.
+› Change printing a builtin function so that it prints its name.
+› Write a function for printing out all the named values in an environment.
+› Redefine one of the builtin variables to something different.
+› Change redefinition of one of the builtin variables to something different an error.
+› Create an exit function for stopping the prompt and exiting.
